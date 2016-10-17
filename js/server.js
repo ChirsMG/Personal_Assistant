8b44b6e64434c902e81fbbe438fd21c27e656932
Chris Glass <Christopher.glass@colorado.edu>
2016-10-17 16:13:42 -0600

    Server side 10/17/16
    added add item funcitonality to server (well... tried to )

--- js/server.js
+++ js/server.js
@@ -1,7 +1,7 @@
 var app= require('express')();
 var server= app.listen(30);
 var io = require('socket.io').listen(server);
-var users=["MASTER"];
+/*var users=["MASTER"];
 var PWDs=["TEMPLOGIN"]; //TODO: hash function
 var MYSQL =require('mysql')
 
@@ -10,9 +10,41 @@ var con = mysql.createConnection({
 	user:"app",
 	password:"Ma1nApp";
 	databse:
-})
+})*/
 
-function
+var Mongo=require("mongodb");
+var MongoClient=Mongo.Client;
+
+
+
+function add_item(db,userID,item){
+	collection=db.collection('items');
+	collection.insert(item,function(err,result){
+		if(err){
+			console.log(err);
+		} else{
+			console.log("successfuly inserted %d documents into 'item' colleciton")
+		}
+	});	
+}
+
+function update_item(db,userID,itemID,update){
+	collection=db.collection('items');
+	collection.update({"_id":itemID},{$set:{update}},function(err,result){
+		if(err){
+			console.log(err);
+		} else if (numUpdated){
+			console.log("successfuly inserted %d documents into 'item' colleciton")
+		}else{
+		 	add_item(db,userID,item)//get item by id?
+		}
+	});
+};
+function asyncDB(db,userID,callback,items){
+	for(var item in items){
+		callback(db,userID,item);
+	}
+}
 
 console.log("running");
 app.get('/',function(req,res){
@@ -41,7 +73,22 @@ io.on('connection',function(socket){
 			//look for tag by name
 			// add tag id item id relationship
 		});
-		socket.on("add_item", function(){
+		socket.on("add_item", function(items){
+			MongoClient.Connect(mongodb:localost:2017/personal_assitant, function(err,db){
+				//TO DO: add asyncronous add item
+
+				if(err){
+					throw err;
+				}else{
+					console.log("database connnected");
+
+					asyncDB(db,00,add_item,items);
+
+				}
+
+
+
+				});
 			//look for tag by name
 			// add tag id item id relationship
 			
