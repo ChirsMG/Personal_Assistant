c94407f299d5c436539441363452eb5050fde239
Chris Glass <Christophermglass@gmail.com>
2017-09-16 23:19:11 -0600

    Added Handler Functionality
    add basic handler and increased logging information. should output more info about request and send 200 status code with hello world text

--- src/server/GatewayHandler.java
+++ src/server/GatewayHandler.java
@@ -3,15 +3,23 @@ package server;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+
 class GatewayHandler implements  HttpHandler{
-
-
 
     GatewayHandler(){
 
     }
+    @Override
+    public void handle(HttpExchange exchange) throws IOException{
+        System.out.println("path: "+exchange.getHttpContext().getPath());
+        System.out.println(exchange.getRequestBody());
 
-    public void handle(HttpExchange exchange){
+        OutputStream responseMessage=exchange.getResponseBody();
 
+        exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK,0);
+        responseMessage.write("Hello World".getBytes());
     }
 }
--- src/server/PersonalAssistantGateway.java
+++ src/server/PersonalAssistantGateway.java
@@ -21,10 +21,11 @@ public class PersonalAssistantGateway{
              server = HttpServer.create(new InetSocketAddress(8000), 0);
              server.createContext("/test", new GatewayHandler());
             LOGGER.info("server context created");
-            LOGGER.info("context: /test");
+            LOGGER.info("CanonicalHostName: "+server.getAddress().getAddress().getCanonicalHostName());
+             LOGGER.info("Host String : "+server.getAddress().getHostString());
             LOGGER.info("address:"+server.getAddress().getAddress().getHostAddress());
-            LOGGER.info("port: ");
-            LOGGER.info("hostName:");
+            LOGGER.info("port: "+server.getAddress().getPort());
+            LOGGER.info("hostName:"+server.getAddress().getHostName());
             server.start();
         }catch(Exception e){
             LOGGER.log(Level.SEVERE,"Exception: "+e.getMessage());
