40bca49400d2c8022171bf8c3e8ced30b98103b2
Chris Glass <Christophermglass@gmail.com>
2017-09-23 13:29:30 -0600

    more fixes around learning java
    may be superfluous

--- src/server/PersonalAssistantGateway.java
+++ src/server/PersonalAssistantGateway.java
@@ -20,7 +20,7 @@ public class PersonalAssistantGateway{
         try {
              server = HttpServer.create(new InetSocketAddress(8000), 0);
              server.createContext("/test", new GatewayHandler());
-            server.start();
+           server.start();
             LOGGER.info("server context created");
             LOGGER.info("CanonicalHostName: "+server.getAddress().getAddress().getCanonicalHostName());
             LOGGER.info("Host String : "+server.getAddress().getHostString());
