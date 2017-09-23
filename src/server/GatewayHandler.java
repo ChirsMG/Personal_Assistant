f9a0ee83d6ba06c3d12a3dfa74f9f2cb7958e0c7
Chris Glass <Christophermglass@gmail.com>
2017-09-23 13:40:44 -0600

    constructor issue

--- src/server/GatewayHandler.java
+++ src/server/GatewayHandler.java
@@ -10,9 +10,7 @@ import java.util.logging.Logger;
 
 class GatewayHandler implements  HttpHandler{
     private final static Logger LOGGER = Logger.getLogger(GatewayHandler.class.getName());
-    GatewayHandler(){
-
-    }
+    
     @Override
     public void handle(HttpExchange exchange) throws IOException{
         LOGGER.info("using handler");
