2cc214dd74f55483a7bc366c84b14427e640ff7b
Chris Glass <Christophermglass@gmail.com>
2017-12-03 12:04:42 -0700

    add storageConnector class

---
+++ src/server/StorageConnector.java
@@ -0,0 +1,11 @@
+package server;
+
+import java.util.Map;
+
+interface StorageConnector {
+
+    Object read(String query);
+    boolean write(Map<String,Object> item);
+    boolean delete(String query);
+    void storageName(String name);
+}
